29b8ac0f41d00337c1a5ebe8c58697e38b3c54a7
Gagan46-creator <gaganshankar46@gmail.com>
2026-04-06 14:18:36 +0530

    feat: add ranking engine to identify largest value

---
+++ ranker.py
@@ -0,0 +1,6 @@
+x = float(input("Enter first value: "))
+y = float(input("Enter second value: "))
+z = float(input("Enter third value: "))
+
+largest = max(x, y, z)
+print("Largest value is:", largest)
